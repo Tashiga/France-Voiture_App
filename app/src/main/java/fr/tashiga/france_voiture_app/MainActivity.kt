093184203735464da2005e60d2cf1d19100565e4
tashiga <sasisashtiga2002@gmail.com>
2022-03-06 21:37:29 +0100

    change text of TextView on onclick button + getConnexion allow for any user of DB

--- app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
+++ app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
@@ -2,11 +2,9 @@ package fr.tashiga.france_voiture_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import java.sql.DriverManager
-import java.util.*
+import java.sql.ResultSet
 
 class MainActivity : AppCompatActivity() {
     internal val mysql = MySQLDB.MySQLDB()
@@ -14,17 +12,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        mysql.executeQuery()
-//        val textView = findViewById<TextView>(R.id.text_view_id) as TextView
-//        val errorText = findViewById<TextView>(R.id.error_text_id) as TextView
-//        val show = findViewById<Button>(R.id.button_id) as Button
-//
-//        show.setOnClickListener ( View.OnClickListener {
-//            @Override
-//            fun onCLick(view: View) {
-//
-//            }
-//        })
+        val textView = findViewById<TextView>(R.id.text_view_id) as TextView
+        val errorText = findViewById<TextView>(R.id.error_text_id) as TextView
+        val show = findViewById<Button>(R.id.button_id) as Button
+
+        show.setOnClickListener {
+//                var result: ResultSet? = mysql.executeQuery("select * from article")
+//                var valeur:String = ""
+//                textView.text = valeur
+//                println("--- LES ARTICLES ---")
+//                while(result!!.next()) {
+//                    valeur +=  "id de l'article : " + result.getString("idArticle") + ", nom : " + result.getString("nom").toUpperCase() + ", categorie : " +result.getString("categorie") + ", prix :  " + result.getString("prix") + " euros, date :  " + result.getString("dateCreation") + " "
+//                    valeur += "description : " + result.getString("description") + " stock : " + result.getString("nbStock") + "\n"
+//                }
+//                result!!.close()
+            textView.text = "hello"
+        }
 
     }
 
--- app/src/main/java/fr/tashiga/france_voiture_app/MySQLDB.kt
+++ app/src/main/java/fr/tashiga/france_voiture_app/MySQLDB.kt
@@ -1,15 +1,18 @@
 package fr.tashiga.france_voiture_app
 
-import java.sql.*
+
+import android.database.SQLException
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
 import java.util.*
 
 object MySQLDB {
     internal var conn: Connection? = null
-    internal var username = "user"
-    internal var password = "mdp"
 
     fun MySQLDB():MySQLDB {
-        getConnection()
+        getConnection("user", "mdp")
         return this
     }
 
@@ -18,24 +21,26 @@ object MySQLDB {
      * In this example, MySQL Server is running in the local host (so 127.0.0.1)
      * at the standard port 3306
      */
-    fun getConnection() {
+    fun getConnection(username: String, password: String): Connection? {
         val connectionProps = Properties()
         connectionProps.put("user", username)
         connectionProps.put("password", password)
         try {
 //            Class.forName("com.mysql.jdbc.Driver").newInstance()
-            conn = DriverManager.getConnection(
+            this.conn = DriverManager.getConnection(
                 "jdbc:" + "mysql" + "://" +
-                        "192.168.1.42" +"/" +
+                        "192.168.1.42" + ":3306" + "/" +
                         "francevoiture",
                 connectionProps)
-
+            return conn!!
         } catch (ex: SQLException) {
             // handle any errors
             ex.printStackTrace()
+            return null
         } catch (ex: Exception) {
             // handle any errors
             ex.printStackTrace()
+            return null
         }
     }
 
@@ -43,6 +48,9 @@ object MySQLDB {
         var stmt: Statement? = null
         var resultset: ResultSet? = null
 
+        getConnection("user", "mdp")
+
+        println("avant execution " + stmt + " connection : " + conn)
         try {
             stmt = conn!!.createStatement()
             resultset = stmt!!.executeQuery("SHOW DATABASES;")
@@ -63,48 +71,107 @@ object MySQLDB {
             }
             println("-------------------------\n")
 
-//            resultset = stmt!!.executeQuery("select * from client;")
-//            println("--- VOICI LA REQUETE ---")
-//            while(resultset!!.next()) {
-//                println(resultset.getArray(resultset.row))
-//            }
-//            println("-------------------------\n")
+            resultset = stmt!!.executeQuery("select * from client;")
+            println("--- VOICI LES CLIENTS ---")
+            while(resultset!!.next()) {
+                println(resultset.getString("civilite") + ". " + resultset.getString("nom").toUpperCase() + " " + resultset.getString("prenom"))
+            }
+            println("-------------------------\n")
+
+            resultset = stmt!!.executeQuery("select * from vendeur;")
+            println("--- VOICI LES VENDEURS ---")
+            while(resultset!!.next()) {
+                println(resultset.getString("civilite") + ". " + resultset.getString("nom").toUpperCase() + " " + resultset.getString("prenom"))
+            }
+            println("-------------------------\n")
 
         }catch (ex: SQLException) {
             // handle any errors
             ex.printStackTrace()
-        }finally {
-            // release resources
-            if (resultset != null) {
-                try {
-                    resultset.close()
-                } catch (sqlEx: SQLException) {
-                }
-                resultset = null
-            }
-            if (stmt != null) {
-                try {
-                    stmt.close()
-                } catch (sqlEx: SQLException) {
-                }
-                stmt = null
-            }
-
-            if (conn != null) {
-                try {
-                    conn!!.close()
-                } catch (sqlEx: SQLException) {
-                }
-                conn = null
-            }
-
         }
+//        finally {
+//            // release resources
+//            if (resultset != null) {
+//                try {
+//                    resultset.close()
+//                } catch (sqlEx: SQLException) {
+//                }
+//                resultset = null
+//            }
+//            if (stmt != null) {
+//                try {
+//                    stmt.close()
+//                } catch (sqlEx: SQLException) {
+//                }
+//                stmt = null
+//            }
+//
+////            if (conn != null) {
+////                try {
+////                    conn!!.close()
+////                } catch (sqlEx: SQLException) {
+////                }
+////                conn = null
+////            }
+//
+//        }
     }
 
-    @JvmStatic fun main(args: Array<String>){
-        val mysql:MySQLDB = MySQLDB()
-//        executer une requete
-        mysql.executeQuery()
-    }
+//    //execute n'importe quel requete et renvoie les resultats
+//    fun executeQuery(query: String):ResultSet? {
+//        var stmt: Statement? = null
+//        var resultset: ResultSet? = null
+//        try {
+//            stmt = this.conn!!.createStatement()
+//            resultset = stmt!!.executeQuery(query)
+//            if (stmt.execute(query)) {
+//                resultset = stmt.resultSet
+//            }
+//        } catch (ex: SQLException) {
+//            println("erreur lors de l'exuction de la requete SQL")
+//        }
+//        return resultset
+//    }
+//
+//    fun getConn(): Connection? {
+//        try {
+//            return this.conn!!
+//        }catch (e: Exception) {
+//            e.printStackTrace()
+//            return null
+//        }
+//    }
+
+//    Exemple d'un main fonctionnel
+
+//    @JvmStatic fun main(args: Array<String>){
+//
+////  Base de donnee : insertion d'articles
+////  categorie enum('pneus','moteur','freinage','systeme electrique', 'amortisseur','echappement', 'suspension', 'piece allumage', 'climatisation', 'carrosserie', 'alternateur', 'filtre', 'direction') not null
+////  INSERT INTO article (nom, prix, description, nbStock, dateCreation, categorie) VALUES('Un pneu', '56.23', null, 2, (select CURRENT_DATE ()), 'pneus');
+//
+//        val mysql:MySQLDB = MySQLDB()
+////      executer une requete
+//        mysql.executeQuery()
+//        var resultats: ResultSet? = mysql.executeQuery("select * from vendeur;")
+//        println("--- NOTRE RESULTAT DE EXECUTEQUERY() ---")
+//        var lign: Int = 1
+//        while(resultats!!.next()) {
+//            println( "" + lign + ". " + resultats.getString("civilite") + ". " + resultats.getString("nom").toUpperCase() + " " + resultats.getString("prenom"))
+//            println("\tMdp : " + resultats.getString("mdp") + "\n\tRaison social : " + resultats.getString("raison_social") + "\n\temail : " + resultats.getString("email") )
+//            lign++
+//        }
+//        println("-------------------------\n")
+//        resultats.close()
+//        resultats = mysql.executeQuery("select * from article")
+//        println("--- LES ARTICLES ---")
+//        lign = 1
+//        while(resultats!!.next()) {
+//            println( "" + lign + ". " + resultats.getString("nom").toUpperCase() + "[" +resultats.getString("categorie") + "] " + resultats.getString("prix") + " euros, ajouté le " + resultats.getString("dateCreation"))
+//            println("\tDescription : " + resultats.getString("description") + "\n\tStock : " + resultats.getString("nbStock") )
+//            lign++
+//        }
+//        resultats.close()
+//    }
 
 }
