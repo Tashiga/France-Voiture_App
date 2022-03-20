accf4a9b25dbca1d8b8949e39864d63c24db1ae2
tashiga <sasisashtiga2002@gmail.com>
2022-03-21 00:06:52 +0100

    Add first interface to show results

--- app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
+++ app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.tashiga.france_voiture_app
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.sql.*
@@ -18,18 +19,19 @@ class MainActivity : AppCompatActivity() {
             try {
                 MySQL.MySQL("user", "mdp")
                 var connection:Connection? = MySQL.connectMySQL()
-                var addText:String? = "Articles : " + "\n"
+                var addText:String? = "\t" + "Articles : " + "\n\n"
                 val sql = "SELECT * FROM article"
                 val resultSet = MySQL.executeRequete(connection!!, sql)
                 while (resultSet!!.next()) {
-                    addText += resultSet.getString("idArticle") + "-" +
+                    addText += resultSet.getString("idArticle") + " - " +
                             resultSet.getString("nom") + " (" +resultSet.getString("categorie") + ") \n" +
                             "Description : " + resultSet.getString("description") + "\n" +
                             "Stock : " + resultSet.getString("nbStock") + "\n" +
-                            " Prix : " + resultSet.getString("prix") + " euros \n" +
+                            "Prix : " + resultSet.getString("prix") + " euros \n" +
                             "Date de création : " +resultSet.getString("dateCreation")  + "\n\n"
                 }
                 text.text = addText
+
             }catch (e:Exception) {
                 e.printStackTrace()
                 erreur.text = e.message
