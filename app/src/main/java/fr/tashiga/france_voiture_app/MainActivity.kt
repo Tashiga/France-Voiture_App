b780defd972add03e2518108c87c6c10efab8472
tashiga <sasisashtiga2002@gmail.com>
2022-03-20 19:56:17 +0100

    Refactor : create a new class for SQL

--- app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
+++ app/src/main/java/fr/tashiga/france_voiture_app/MainActivity.kt
@@ -1,8 +1,6 @@
 package fr.tashiga.france_voiture_app
 
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,10 +16,11 @@ class MainActivity : AppCompatActivity() {
         var erreur = findViewById<TextView>(R.id.error_text_id) as TextView
         boutonArticle.setOnClickListener {
             try {
-                var connection:Connection? = connectMySQL()
+                MySQL.MySQL("user", "mdp")
+                var connection:Connection? = MySQL.connectMySQL()
                 var addText:String? = "Articles : " + "\n"
                 val sql = "SELECT * FROM article"
-                val resultSet = executeRequete(connection!!, sql)
+                val resultSet = MySQL.executeRequete(connection!!, sql)
                 while (resultSet!!.next()) {
                     addText += resultSet.getString("idArticle") + "-" +
                             resultSet.getString("nom") + " (" +resultSet.getString("categorie") + ") \n" +
@@ -35,63 +34,6 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
                 erreur.text = e.message
             }
-        }
-    }
-
-    /**
-     * connection à MySQL
-     */
-    fun connectMySQL(): Connection? {
-        var cn: Connection?
-        try {
-            Class.forName("com.mysql.jdbc.Driver")
-            val policy = ThreadPolicy.Builder()
-                .permitAll().build()
-            StrictMode.setThreadPolicy(policy)
-            cn = DriverManager.getConnection("jdbc:mysql://192.168.1.42/francevoiture",
-                "user", "mdp")
-            return cn
-        } catch (e: ClassNotFoundException) {
-            e.printStackTrace()
-            return null
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            return null
-        }
-    }
-
-    /**
-     * executer une requete SQL
-     */
-    fun executeRequete(cn:Connection, sql:String) : ResultSet?{
-        try {
-            val ps = cn.createStatement()
-            val resultSet = ps!!.executeQuery(sql)
-            return resultSet
-        } catch (e: ClassNotFoundException) {
-            e.printStackTrace()
-            return null
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            return null
-        }
-    }
-
-    /**
-     * femmer le statement utiliser pour executer une requete
-     */
-    fun fermerStatement(ps:Statement) {
-        if (ps != null) {
-            ps!!.close()
-        }
-    }
-
-    /**
-     * fermer la connection de la base de données
-     */
-    fun deconnexionMySQL (cn:Connection) {
-        if (cn != null) {
-            cn.close()
         }
     }
 
---
+++ app/src/main/java/fr/tashiga/france_voiture_app/MySQL.kt
@@ -0,0 +1,74 @@
+package fr.tashiga.france_voiture_app
+
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import java.sql.*
+
+object MySQL {
+
+    private var user:String = ""
+    private var pass: String = ""
+
+    fun MySQL(user:String, pass:String) {
+        this.pass = pass
+        this.user = user
+    }
+
+    /**
+     * connection à MySQL
+     */
+    fun connectMySQL(): Connection? {
+        var cn: Connection?
+        try {
+            Class.forName("com.mysql.jdbc.Driver")
+            val policy = StrictMode.ThreadPolicy.Builder()
+                .permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            cn = DriverManager.getConnection("jdbc:mysql://192.168.1.42/francevoiture",
+                this.user, this.pass)
+            return cn
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            return null
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    /**
+     * executer une requete SQL
+     */
+    fun executeRequete(cn: Connection, sql:String) : ResultSet?{
+        try {
+            val ps = cn.createStatement()
+            val resultSet = ps!!.executeQuery(sql)
+            return resultSet
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            return null
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    /**
+     * femmer le statement utiliser pour executer une requete
+     */
+    fun fermerStatement(ps: Statement) {
+        if (ps != null) {
+            ps!!.close()
+        }
+    }
+
+    /**
+     * fermer la connection de la base de données
+     */
+    fun deconnexionMySQL (cn: Connection) {
+        if (cn != null) {
+            cn.close()
+        }
+    }
+
+}
